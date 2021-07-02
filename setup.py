ad541b3db9c3f974113a34608be51ba0865e3fae
Mark Qvist <mark@unsigned.io>
2021-07-02 23:29:15 +0200

    Vresion bump. Relaxed required python version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lxmf",
-    version="0.0.6",
+    version="0.0.7",
     author="Mark Qvist",
     author_email="mark@unsigned.io",
     description="Lightweight Extensible Message Format for Reticulum",
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=['rns>=0.2.1'],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
