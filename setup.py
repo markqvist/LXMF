1c5e879abdeada61f85800c7506b621d02b2eaaa
Mark Qvist <mark@unsigned.io>
2021-10-02 22:03:58 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lxmf",
-    version="0.0.9",
+    version="0.1.0",
     author="Mark Qvist",
     author_email="mark@unsigned.io",
     description="Lightweight Extensible Message Format for Reticulum",
