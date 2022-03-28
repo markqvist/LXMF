853e70dcbeb854acc8e27135e435ee4005673096
Mark Qvist <mark@unsigned.io>
2022-03-28 15:19:43 +0200

    Dependency bump

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['rns>=0.3.3'],
+    install_requires=['rns>=0.3.4'],
     python_requires='>=3.6',
 )
