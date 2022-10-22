a1dae1fc15d77bcd67df74e245b6cc1baf46e5a9
Mark Qvist <mark@unsigned.io>
2022-10-22 14:35:24 +0200

    Updated versions

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['rns>=0.3.16'],
+    install_requires=['rns>=0.3.17'],
     python_requires='>=3.6',
 )
