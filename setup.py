0ac4660510da8ffa0476d3114abe39212a4de59c
Mark Qvist <mark@unsigned.io>
2023-10-07 16:43:33 +0200

    Updated version and dependencien

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setuptools.setup(
             'lxmd=LXMF.Utilities.lxmd:main',
         ]
     },
-    install_requires=['rns>=0.5.9'],
+    install_requires=['rns>=0.6.2'],
     python_requires='>=3.7',
 )
