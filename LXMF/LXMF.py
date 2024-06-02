a810be316fc53bbbca6e0085e32381bdca465141
Mark Qvist <mark@unsigned.io>
2024-06-02 09:19:47 +0200

    Added audio modes

--- LXMF/LXMF.py
+++ LXMF/LXMF.py
@@ -15,3 +15,27 @@ FIELD_THREAD           = 0x08
 FIELD_COMMANDS         = 0x09
 FIELD_RESULTS          = 0x0A
 FIELD_GROUP            = 0x0B
+
+# Audio modes for the data structure in FIELD_AUDIO
+
+# Codec2 Audio Modes
+AM_CODEC2_450PWB       = 0x01
+AM_CODEC2_450          = 0x02
+AM_CODEC2_700C         = 0x03
+AM_CODEC2_1200         = 0x04
+AM_CODEC2_1300         = 0x05
+AM_CODEC2_1400         = 0x06
+AM_CODEC2_1600         = 0x07
+AM_CODEC2_2400         = 0x08
+AM_CODEC2_3200         = 0x09
+
+# Opus Audio Modes
+AM_OPUS_LBW            = 0x10
+AM_OPUS_MBW            = 0x11
+AM_OPUS_PTT            = 0x12
+AM_OPUS_RT_HDX         = 0x13
+AM_OPUS_RT_FDX         = 0x14
+AM_OPUS_STANDARD       = 0x15
+AM_OPUS_HQ             = 0x16
+AM_OPUS_BROADCAST      = 0x17
+AM_OPUS_LOSSLESS       = 0x18
