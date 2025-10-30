3f91e44a6d6ffec26e7c74e5c434b854e60b1cee
Mark Qvist <mark@unsigned.io>
2025-10-30 14:10:14 +0100

    Updated RNS version

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setuptools.setup(
             'lxmd=LXMF.Utilities.lxmd:main',
         ]
     },
-    install_requires=["rns>=1.0.0"],
+    install_requires=["rns>=1.0.1"],
     python_requires=">=3.7",
 )
