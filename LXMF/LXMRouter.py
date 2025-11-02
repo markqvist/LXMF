e1905b85d7de82d92bab2a278795270c9abecbbe
bus <busimus@gmail.com>
2025-11-02 08:12:33 +0000

    Fix stored messages always getting deleted if config path has underscores

--- LXMF/LXMRouter.py
+++ LXMF/LXMRouter.py
@@ -1090,9 +1090,10 @@ class LXMRouter:
         for transient_id in self.propagation_entries.copy():
             entry = self.propagation_entries[transient_id]
             filepath = entry[1]
-            components = filepath.split("_")
+            filename = os.path.split(filepath)[-1]
+            components = filename.split("_")
 
-            if len(components) == 2 and float(components[1]) > 0 and len(os.path.split(components[0])[1]) == (RNS.Identity.HASHLENGTH//8)*2:
+            if len(components) == 2 and float(components[1]) > 0 and len(components[0]) == (RNS.Identity.HASHLENGTH//8)*2:
                 timestamp = float(components[1])
                 if now > timestamp+LXMRouter.MESSAGE_EXPIRY:
                     RNS.log("Purging message "+RNS.prettyhexrep(transient_id)+" due to expiry", RNS.LOG_EXTREME)
