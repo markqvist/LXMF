84613f8b44693a4b331465d8f397d02f0661903e
Mark Qvist <bc7291552be7a58f361522990465165c>
2026-05-20 19:56:35 +0200

    Updated versions

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setuptools.setup(
             'lxmd=LXMF.Utilities.lxmd:main',
         ]
     },
-    install_requires=["rns>=1.2.5"],
+    install_requires=["rns>=1.2.9"],
     python_requires=">=3.7",
 )
